c00d818537999f8538c8607ab2f963b45b09b710
Janelle Kuhns <j3ckuhns@gmail.com>
2020-02-21 11:40:03 -0800

    started first function

--- nested.py
+++ nested.py
@@ -3,20 +3,42 @@
 """
 Module docstring: One line description of what your program does.
 """
-__author__ = "???"
+__author__ = "Janelle Kuhns"
 
 import sys
 
 
 def is_nested(line):
+    stack = []
+    for l in line:
+        if "(" in l:
+            stack.append('(')
+        else:
+            stack.pop()
+            if '{' in l:
+                stack.append('{')
+            else:
+                stack.pop()
+                if '[' in l:
+                    stack.append('[')
+                else:
+                    stack.pop()
+                
+    if len(stack) == 0:
+        return "Yes"
+    else:
+        return "No"
     """Validate a single input line for correct nesting"""
-    pass
+    
 
 
 def main(args):
+    with open('output.txt', 'r') as file:
+        for line in file:
+            data = file.readlines(line)
+        return data
     """Open the input file and call `is_nested()` for each line"""
     # Results: print to console and also write to output file
-    pass
 
 
 if __name__ == '__main__':
